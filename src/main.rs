1776a50d4927896b007cf2e1b6e0dc36f1762ade
Chris Wong <lambda.fairy@gmail.com>
2015-11-06 18:43:51 +1300

    Rewrite everything

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 #![plugin(maud_macros)]
 
 extern crate iron;
-extern crate iron_maud;
 extern crate maud;
+#[macro_use] extern crate mime;
 extern crate router;
 extern crate serde;
 extern crate serde_json;
 
 use iron::prelude::*;
 use iron::status;
+use mime::Mime;
 use router::Router;
+use std::fmt;
 
 mod user;
 mod views;
@@ -24,9 +26,9 @@ fn main() {
     router.get("*", not_found);
 
     fn home(_: &mut Request) -> IronResult<Response> {
-        let result = views::default(
-            "Karkinos".to_owned(),
-            views::home());
+        let result = make_response(
+            |w| views::default(w, "Karkinos",
+                |w| views::home(w)));
         Ok(Response::with((status::Ok, result)))
     }
 
@@ -35,15 +37,15 @@ fn main() {
         let name = route.find("name").unwrap();
         match User::lookup(name) {
             Ok(user) => {
-                let result = views::default(
-                    name.to_owned(),
-                    views::user(user));
+                let result = make_response(
+                    |w| views::default(w, name,
+                        |w| views::user(w, &user)));
                 Ok(Response::with((status::Ok, result)))
             }
             Err(_) => {
-                let result = views::default(
-                    name.to_owned(),
-                    views::user_not_found(name.to_owned()));
+                let result = make_response(
+                    |w| views::default(w, name,
+                        |w| views::user_not_found(w, name)));
                 Ok(Response::with((status::NotFound, result)))
             },
         }
@@ -51,12 +53,21 @@ fn main() {
 
     fn not_found(r: &mut Request) -> IronResult<Response> {
         // FIXME: use path
-        let result = views::default(
-            "Not Found".to_owned(),
-            views::not_found(r.url.to_string()));
+        let result = make_response(
+            |w| views::default(w, "Not Found",
+                |w| views::not_found(w, &r.url.to_string())));
         Ok(Response::with((status::NotFound, result)))
     }
 
     println!("Starting on port 8344...");
     Iron::new(router).http("localhost:8344").unwrap();
+}
+
+fn make_response<F>(callback: F) -> (Mime, String) where
+    F: FnOnce(&mut fmt::Write) -> fmt::Result
+{
+    let mime = mime!(Text/Html; Charset=Utf8);
+    let mut s = String::new();
+    callback(&mut s).unwrap();
+    (mime, s)
 }
--- src/views.rs
+++ src/views.rs
@@ -1,25 +1,44 @@
 use std::fmt;
 
-use iron_maud::Template;
+use maud::PreEscaped;
 
-pub fn default(title: String, body: Template) -> Template {
-    Template::new(move |w| html!(*w, {
-        $$"<!DOCTYPE html>"
+use user::User;
+
+static STYLES: &'static str = r#"
+html {
+    font-family: "Comic Sans MS", sans-serif;
+}
+h1 {
+    color: #f90;
+    font-size: 500%;
+    letter-spacing: 0.75em;
+    margin: 1rem 0;
+    text-shadow: 2px 2px #000, -2px -2px #faa;
+    text-transform: uppercase;
+}
+"#;
+
+pub fn default<F>(w: &mut fmt::Write, title: &str, body: F) -> fmt::Result where
+    F: FnOnce(&mut fmt::Write) -> fmt::Result
+{
+    html!(*w, {
+        $PreEscaped("<!DOCTYPE html>")
         html {
             head {
                 title $title
-                style $$ r#"html { font-family: "Comic Sans MS" }"#
+                style $PreEscaped(STYLES)
             }
             body {
-                h1 $title
+                h1 "Karkinos"
+                h2 $title
                 #call body
             }
         }
-    }))
+    })
 }
 
-pub fn home() -> Template {
-    Template::new(move |w| html!(*w, {
+pub fn home(w: &mut fmt::Write) -> fmt::Result {
+    html!(*w, {
         p {
             b "Karkinos"
             " is a list of people interested in the "
@@ -31,32 +50,32 @@ pub fn home() -> Template {
             a href="http://rustaceans.org" "rustaceans.org"
             ", but with a different interface."
         }
-    }))
+    })
 }
 
-pub fn not_found(url: String) -> Template {
-    Template::new(move |w| html!(*w, {
+pub fn not_found(w: &mut fmt::Write, url: &str) -> fmt::Result {
+    html!(*w, {
         p {
             "The page at "
             strong $url
             " could not be found."
         }
         p a href="/" "<< Back to home page"
-    }))
+    })
 }
 
-pub fn user(user: User) -> Template {
-    let make_row = |key: &'static str, value: Option<String>| move |w: &mut fmt::Write|
-        html!(*w, #if let Some(ref value) = value {
+pub fn user(w: &mut fmt::Write, user: &User) -> fmt::Result {
+    let make_row = |w: &mut fmt::Write, key: &str, value: &Option<String>|
+        html!(*w, #if let Some(ref value) = *value {
             tr {
                 th $key
                 td $value
             }
         });
-    Template::new(move |w| html!(*w, {
+    html!(*w, {
         table {
-            #call make_row("Name", user.name)
-            #call make_row("IRC nick", user.irc)
+            #call (|w| make_row(w, "Name", &user.name))
+            #call (|w| make_row(w, "IRC nick", &user.irc))
             #if !user.irc_channels.is_empty() {
                 tr {
                     th "IRC channels"
@@ -66,20 +85,20 @@ pub fn user(user: User) -> Template {
                     }
                 }
             }
-            #call make_row("Email", user.email)
-            #call make_row("Discourse", user.discourse)
-            #call make_row("Notes", user.notes)
+            #call (|w| make_row(w, "Email", &user.email))
+            #call (|w| make_row(w, "Discourse", &user.discourse))
+            #call (|w| make_row(w, "Notes", &user.notes))
         }
-    }))
+    })
 }
 
-pub fn user_not_found(name: String) -> Template {
-    Template::new(move |w| html!(*w, {
+pub fn user_not_found(w: &mut fmt::Write, name: &str) -> fmt::Result {
+    html!(*w, {
         p {
             "The user "
             strong $name
             " could not be found."
         }
         p a href="/" "<< Back to home page"
-    }))
+    })
 }
