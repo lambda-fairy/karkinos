8e6d260b5dcd790201460fbe43932fd17322048d
Chris Wong <lambda.fairy@gmail.com>
2016-10-25 16:27:25 +1300

    Ignore empty fields

--- src/user.rs
+++ src/user.rs
@@ -5,6 +5,8 @@ use std::path::PathBuf;
 
 #[derive(Deserialize, Debug)]
 pub struct User {
+    // NOTE: when changing these fields, be sure to update
+    // `.remove_empty_strings()` below
     pub name: Option<String>,
     pub irc: Option<String>,
     pub irc_channels: Vec<String>,
@@ -27,9 +29,36 @@ impl User {
             path
         };
         let reader = BufReader::new(File::open(path)?);
-        let user = serde_json::from_reader(reader)?;
+        let mut user: User = serde_json::from_reader(reader)?;
+        user.remove_empty_strings();
         Ok(user)
     }
+
+    /// Some users use empty strings to mean "not applicable", instead of using
+    /// `null` or omitting the field as they should be doing. We fix up their
+    /// carelessness here.
+    fn remove_empty_strings(&mut self) {
+        macro_rules! fixup {
+            ($self_:ident, $($field:ident)*) => {
+                $(
+                    // If the field is just whitespace, replace it with `None`
+                    // This boolean dance is needed to satisfy borrowck
+                    let should_replace = match $self_.$field {
+                        Some(ref s) if is_whitespace(s) => true,
+                        _ => false,
+                    };
+                    if should_replace {
+                        $self_.$field = None;
+                    }
+                )*
+            }
+        }
+        fixup!(self, name irc email discourse reddit twitter blog website notes);
+    }
+}
+
+fn is_whitespace(s: &str) -> bool {
+    s.chars().all(char::is_whitespace)
 }
 
 #[test]
