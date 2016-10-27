e4850de9a9b7cc027990c6cb91e56b9940e9133e
Chris Wong <lambda.fairy@gmail.com>
2016-10-27 17:15:16 +1300

    Make update-data.sh work on macOS

--- update-data.sh
+++ update-data.sh
@@ -2,12 +2,17 @@
 
 set -e
 
-cd "$(readlink -f "$(dirname "$0")")"
+# Make sure we're in the right directory
+# macOS doesn't support `readlink -f`, so we can't use it here :(
+# cd "$(readlink -f "$(dirname "$0")")"
+cd "$(dirname "$0")"
 
 if ! [ -d rustaceans.org ]
 then
+    # Clone the data from GitHub
     git clone https://github.com/nrc/rustaceans.org.git
 else
+    # Update the existing repo
     cd rustaceans.org
     git fetch
     git checkout master
